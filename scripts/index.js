7696b4a783584f187ccec210336e7f87728ef972
Esraa Abdelhady <esraaabdou94@gmail.com>
2021-03-09 20:33:47 +0000

    add data cells

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,6 @@ xhttp.onreadystatechange = function() {
     if(this.readyState==4 && this.status==200){ 
         const dataset = JSON.parse(this.responseText);
         drawChart(dataset);
-        console.log(dataset)
     }      
 };
 
@@ -14,6 +13,7 @@ function drawChart(dataset) {
    const padding = 60;
    const w = 1200;
    const h = 600;
+   const baseTemp = dataset.baseTemperature;
   
    // create SVG
    const svg = d3.select("div")
@@ -33,12 +33,8 @@ function drawChart(dataset) {
 
    const yScale = d3.scaleLinear()
                     .domain([d3.min(dataset.monthlyVariance, (d) => d.month), d3.max(dataset.monthlyVariance, (d) => d.month)])
-                    .range([h - padding, padding]);
-                    
-   // const yScale = d3.scaleTime()
-                  //   .domain(d3.extent(dataset, time2Date).reverse())
-                  //   .range([h - padding, padding]);
-   
+                    .range([h - 2 * padding, 2 * padding]);
+                      
    // adding axes
    const months = [ "January", "February", "March", "April", "May", "June", 
    "July", "August", "September", "October", "November", "December" ];
@@ -46,9 +42,8 @@ function drawChart(dataset) {
    const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
    const yAxis = d3.axisLeft(yScale).tickFormat(d => months[d-1]);
 
-   
    svg.append("g")
-      .attr("transform", "translate(0," + (h - padding) + ")")
+      .attr("transform", "translate(0," + (h - 2* padding) + ")")
       .attr("id", "x-axis")
       .call(xAxis);
 
@@ -66,7 +61,7 @@ function drawChart(dataset) {
       .call(yAxis);
    
    svg.append("text")
-      .attr("y", h - padding/2)
+      .attr("y", h - 2 * padding + 20)
       .attr("x",  w/2)
       .attr("dy", "1em")
       .style("text-anchor", "middle")
@@ -88,5 +83,32 @@ function drawChart(dataset) {
    .attr("dy", "1em")
    .attr("id","description")
    .style("text-anchor", "middle")
-   .text(`${minYear} - ${maxYear}: base temperature ${dataset.baseTemperature}℃`);
+   .text(`${minYear} - ${maxYear}: base temperature ${baseTemp}℃`);
+
+   // adding dots
+   svg.selectAll("rect")
+      .data(dataset.monthlyVariance)
+      .enter()
+      .append("rect")
+      .attr("data-year", d => d.year)
+      .attr("data-month", d => d.month -1)
+      .attr("data-temp", d => d.variance)
+      .attr("x", d => xScale(d.year))
+      .attr("y", d => yScale(d.month + 1))
+      .attr("width", d => xScale(d.year + 1) - xScale(d.year))
+      .attr("height", d => yScale(d.month ) - yScale(d.month + 1))
+      .attr("class", "cell")
+      .attr("fill", d => {
+         const temp = baseTemp + d.variance;
+         if(temp > 11.7) return "#D73027";
+         if(temp > 10.6) return "#F46D43";
+         if(temp > 9.5) return "#FDAE61";
+         if(temp > 8.3) return "#FEE090";
+         if(temp > 7.2) return "#FFFFBF";
+         if(temp > 6.1) return "#E0F3F8";
+         if(temp > 5.0) return "#ABD9E9";
+         if(temp > 3.9) return "#74ADD1";
+         if(temp > 2.8) return "#4575B4";
+         else return "#FFFFFF";
+      })
 }
